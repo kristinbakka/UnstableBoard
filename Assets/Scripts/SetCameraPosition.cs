df8535151f3ecdc4cd3dc553b3abd806e228b6e7
Kristin <kbbakka@gmail.com>
2021-02-27 11:11:48 +0100

    First Commit

---
+++ Assets/Scripts/SetCameraPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraPosition : MonoBehaviour
+{
+    public Rigidbody Player;
+    //This is the object that determines whare the camera is focusing
+    public Rigidbody CameraCenterObject;
+    private bool FollowPlayerOn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerHeight = Player.transform.position.y;
+        if (playerHeight == 10)
+        {
+            CameraCenterObject.transform.position = new Vector3(0, 0, 0);
+            CameraCenterObject.velocity = Vector3.zero;
+        }
+        
+        if (playerHeight <= -10)
+        {
+            CameraCenterObject.velocity = Vector3.up * Player.velocity.y;
+        }
+            
+        
+    }
+}
---
+++ Assets/Scripts/SetRotationOfPlane.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Quaternion = System.Numerics.Quaternion;
+
+public class SetRotationOfPlane : MonoBehaviour
+{
+    public GameObject GameBoard;
+    // set rotation component here, and make it somewhat.
+    private float SpeedX = 5;
+    private float SpeedZ = 5;
+    private float RotationX;
+    private float RotationZ;
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpeedX = 5;
+        SpeedZ = 5;
+        RotationX = 0;
+        RotationZ = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotationX = SpeedX * Input.GetAxis("Mouse X");
+        RotationZ = SpeedZ * Input.GetAxis("Mouse Y");
+        GameBoard.transform.Rotate(-RotationZ, 0, RotationX);
+
+        if (Player.transform.position.y <= -50)
+            Player.transform.position = new Vector3(0, 50, 0);
+    }
+}
